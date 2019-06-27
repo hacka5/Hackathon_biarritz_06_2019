699eab21643f0d699f7328ad89cf0e1c8170eabb
wilder <gagnantnicolas64@gmail.com>
2019-06-27 22:02:29 +0200

    create background and create home

--- front/src/components/home/Home.jsx
+++ front/src/components/home/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+import './Home.css';
 
 
 function Home() {
   return (
+    <body>
+
     <Container>
-      <h1>Welcome to the inter-campus hackathon competition</h1>
+          <h1>Welcome to the inter-campus hackathon competition</h1>
       <h3>Present by the Hacka5 team!</h3>
     </Container>
+    </body>
+    
   );
 }
 
