247ee836b0b72908f5b8b2afc694818459755365
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-06-27 16:13:01 +0200

    turn the eloMap into an object to make the route /ranking works

--- server/index.js
+++ server/index.js
@@ -8,9 +8,10 @@ const campuses = require('./routes/campuses');
 const matches = require('./routes/matches');
 const elo = require('./routes/elo');
 
-app.use('/students', students);
-app.use('/campuses', campuses);
-app.use('/matches', matches);
+app.use(express.json());
+// app.use('/students', students);
+// app.use('/campuses', campuses);
+// app.use('/matches', matches);
 app.use('/ranking', elo);
 
 const port = process.env.PORT || 3001;
