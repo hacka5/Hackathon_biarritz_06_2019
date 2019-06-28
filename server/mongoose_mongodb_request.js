81a2388ac4f09d7cfbd63efcb5fd54de7becd5c3
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-06-28 11:44:12 +0200

    tried to sort the dates

--- server/mongoose_mongodb_request.js
+++ server/mongoose_mongodb_request.js
@@ -1,22 +1,19 @@
-// const express = require('express');
-// const router = express.Router();
-// const axios = require('axios');
-// const mongoose = require('mongoose');
+const express = require('express');
+const axios = require('axios');
+const mongoose = require('mongoose');
 
-// mongoose.set('useNewUrlParser', true);
-// mongoose.set('useFindAndModify', false);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
 
-// //MONGOOSE CONNECT
-// mongoose
-// 	.connect('mongodb://127.0.0.1:27017/hackathon2')
-// 	.then(() => console.log('Connected succesfully to mongoDB...'))
-// 	.catch(err => console.error('Could not connect to mongoDB...', err));
+//MONGOOSE CONNECT
+mongoose
+	.connect('mongodb://127.0.0.1:27017/hackathon2')
+	.then(() => console.log('Connected succesfully to mongoDB...'))
+	.catch(err => console.error('Could not connect to mongoDB...', err));
 
 // const campuses = require('./campuses');
-// const matches = require('./matches');
+const matches = require('./schemas/matches');
 // const students = require('./students');
-
-// let dataCampuses;
 
 // // // const createCampus = async (namE, citY, countrY, uiD, creatioN, firstName, lastName) => {
 // // // 	const campus = new campuses({
@@ -34,15 +31,15 @@
 // // // 	await campus.save();
 // // // };
 
-// const createMatches = async (hometeam, awayteam, isodate, winneruid) => {
-// 	const match = new matches({
-// 		homeTeam: hometeam,
-// 		awayTeam: awayteam,
-// 		isoDate: isodate,
-// 		winnerUid: winneruid
-// 	});
-// 	await match.save();
-// };
+const createMatches = async (hometeam, awayteam, isodate, winneruid) => {
+	const match = new matches({
+		homeTeam: hometeam,
+		awayTeam: awayteam,
+		isoDate: isodate,
+		winnerUid: winneruid
+	});
+	await match.save();
+};
 
 // // const createStudents = async (firstName, lastName, campuS) => {
 // // 	const student = new students({
@@ -53,22 +50,22 @@
 // // 	await student.save();
 // // };
 
-// const getData = async () => {
-// 	try {
-// 		return await axios
-// 			.get('https://us-central1-rc-league.cloudfunctions.net/wildcodeschool/matches')
-// 			.then(res => {
-// 				res.data.map(async e => {
-// 					await createMatches(e.homeTeam, e.awayTeam, e.isoDate, e.winnerUid);
-// 				});
-// 			})
-// 			.finally(() => {});
-// 	} catch (error) {
-// 		console.error(error);
-// 	}
-// };
+const getData = async () => {
+	try {
+		return await axios
+			.get('https://us-central1-rc-league.cloudfunctions.net/wildcodeschool/matches')
+			.then(res => {
+				res.data.map(async e => {
+					await createMatches(e.homeTeam, e.awayTeam, e.isoDate, e.winnerUid);
+				});
+			})
+			.finally(() => {});
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-// getData();
+getData();
 
 // // router.get('/', async (req, res) => {
 // // 	await res.send(dataCampuses);
