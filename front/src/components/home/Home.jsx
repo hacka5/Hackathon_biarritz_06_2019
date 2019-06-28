0ce1a93c6d4cf9e2e0bb2d799c9a44612cafb73b
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-06-28 10:12:25 +0200

    changed the body background

--- front/src/components/home/Home.jsx
+++ front/src/components/home/Home.jsx
@@ -4,14 +4,12 @@ import './Home.css';
 
 function Home() {
 	return (
-		<body>
-			<Container>
-				<header>
-					<h1 className='titre'>Welcome to the inter-campus hackathon competition</h1>
-					<h3 className='sousTitre'>Present by the Hacka5 team!</h3>
-				</header>
-			</Container>
-		</body>
+		<Container className='bckgrd'>
+			<header>
+				<h1 className='titre'>Welcome to the inter-campus hackathon competition</h1>
+				<h3 className='sousTitre'>Present by the Hacka5 team!</h3>
+			</header>
+		</Container>
 	);
 }
 
