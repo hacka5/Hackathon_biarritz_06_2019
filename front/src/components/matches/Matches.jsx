fe34c159cab644d9a0a9b70a3f621b53ce9e2a8a
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-06-28 10:01:25 +0200

    debbuged the card in matches component which weren't the right size

--- front/src/components/matches/Matches.jsx
+++ front/src/components/matches/Matches.jsx
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
-import Match from "./MatchesCards";
+import React, { Component } from 'react';
+import Match from './MatchesCards';
 import { Row, Col, Container } from 'reactstrap';
 import Axios from 'axios';
 
-
 class Matches extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataMatches: [],
-    };
-  }
-  
-  componentDidMount() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			dataMatches: []
+		};
+	}
+
+	componentDidMount() {
 		Axios.get('http://localhost:3002/matches')
 			.then(res => {
 				this.setState({
@@ -22,23 +21,20 @@ class Matches extends Component {
 			.catch(e => console.log(e));
 	}
 
-  render() {
-    return (
-      <Container style={{marginBottom:100}}>
-      <h1 style={{textAlign:'center', paddingTop:60, marginBottom:60}}>Inter-campus matches</h1>
-      <Row>
-      
-      {this.state.dataMatches.map((eachMatch, i) => (
-        <div key={i}>
-        <Col xs={12} md={6} lg={4} style={{paddingBottom:40}}><Match {...eachMatch} /></Col>
-        </div>
-      ))}
-        
-        </Row>
-
-        </Container>
-    );
-  }
+	render() {
+		return (
+			<Container style={{ marginBottom: 100 }}>
+				<h1 style={{ textAlign: 'center', paddingTop: 60, marginBottom: 60 }}>Inter-campus matches</h1>
+				<Row>
+					{this.state.dataMatches.map((eachMatch, i) => (
+						<Col xs={12} md={6} lg={4} style={{ paddingBottom: 40 }}>
+							<Match {...eachMatch} />
+						</Col>
+					))}
+				</Row>
+			</Container>
+		);
+	}
 }
 
 export default Matches;
