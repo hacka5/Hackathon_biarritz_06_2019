6495c229b2c77ffbcd356d80a0a59066bedcd9c8
wilder <gagnantnicolas64@gmail.com>
2019-06-28 09:33:49 +0200

    home final

--- front/src/components/home/Home.jsx
+++ front/src/components/home/Home.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Container } from 'reactstrap';
-import './Home.css';
-
+import React from "react";
+import { Container } from "reactstrap";
+import "./Home.css";
 
 function Home() {
   return (
     <body>
-
-    <Container>
-          <h1>Welcome to the inter-campus hackathon competition</h1>
-      <h3>Present by the Hacka5 team!</h3>
-    </Container>
+      <Container>
+        <header>
+          <h1 className="titre">
+            Welcome to the inter-campus hackathon competition
+          </h1>
+          <h3 className="sousTitre">Present by the Hacka5 team!</h3>
+        </header>
+      </Container>
     </body>
-    
   );
 }
 
