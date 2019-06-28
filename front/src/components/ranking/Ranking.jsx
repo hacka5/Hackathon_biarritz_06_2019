318c9f2400ffff0834e76a3fcdc0966cc0075412
Vanessa Salvador <salvadoribanez.vanessa@gmail.com>
2019-06-28 11:01:28 +0200

    Created Ranking component with  temporary data, in an array, waiting to activate the server and use axios (this part is commented in the code)

--- front/src/components/ranking/Ranking.jsx
+++ front/src/components/ranking/Ranking.jsx
@@ -1,35 +1,126 @@
 import React, { Component } from 'react';
-import Axios from 'axios';
+import { Table, Container } from 'reactstrap';
 
-class Matches extends Component {
+//import Axios from 'axios';
+
+class Ranking extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			dataMatches: []
+			dataRanking: [
+				{
+					"team": "bia",
+					"elo": 1019
+				  },
+				  {
+					"team": "bor",
+					"elo": 1024
+				  },
+				  {
+					"team": "lon",
+					"elo": 1010
+				  },
+				  {
+					"team": "ber",
+					"elo": 999
+				  },
+				  {
+					"team": "buc",
+					"elo": 997
+				  },
+				  {
+					"team": "mar",
+					"elo": 1015
+				  },
+				  {
+					"team": "lis",
+					"elo": 969
+				  },
+				  {
+					"team": "mad",
+					"elo": 982
+				  },
+				  {
+					"team": "lil",
+					"elo": 1016
+				  },
+				  {
+					"team": "toul",
+					"elo": 1016
+				  },
+				  {
+					"team": "par",
+					"elo": 966
+				  },
+				  {
+					"team": "stra",
+					"elo": 984
+				  },
+				  {
+					"team": "bru",
+					"elo": 970
+				  },
+				  {
+					"team": "tour",
+					"elo": 1015
+				  }
+				 ]
 		};
 	}
 
+	// componentDidMount() {
+	// 	Axios.get('http://localhost:3002/ranking')
+	// 		.then(res => {
+	// 			this.setState({
+	// 				dataRanking: res.data
+	// 			});
+	// 		})
+	// 		.catch(e => console.log(e));
+	// }
+
 	componentDidMount() {
-		Axios.get('http://localhost:3002/ranking')
-			.then(res => {
-				this.setState({
-					dataMatches: res.data
-				});
+		// API request, passing the url
+	
+			fetch(this.state.dataRanking)
+			.then(res => res.json())
+			.then((data) => {
+			  this.setState({ dataRanking: data })
 			})
-			.catch(e => console.log(e));
-	}
+			.catch(console.log);       
+		  }
 
 	render() {
-		const { dataMatches } = this.state;
-		console.log(this.state.dataMatches);
+		const { dataRanking } = this.state;
+		console.log(this.state.dataRanking);
+
 		return (
-			<div>
-				<ul>
-					<li>{dataMatches.bia}</li>
-				</ul>
-			</div>
+
+			<Container style={{ marginBottom: 100 }}>
+				<h1 style={{ textAlign: 'center', paddingTop: 60, marginBottom: 60 }}>Campus Ranking</h1>
+					
+						<Table>
+						<thead>
+						  <tr>
+							<th>Position</th>
+							<th>Campus</th>
+							<th>Score</th>
+						  </tr>
+						</thead>
+						{dataRanking.sort((a,b)=>{return b.elo-a.elo}).map((eachRanking, i) => (
+						<tbody>
+						  <tr>
+							<th scope="row">{i+1}</th>
+							<td>{eachRanking.team}</td>
+							<td>{eachRanking.elo}</td>
+						  </tr>
+						</tbody>))}
+					  </Table>
+					
+
+			</Container>
+			
 		);
 	}
 }
 
-export default Matches;
+export default Ranking;
