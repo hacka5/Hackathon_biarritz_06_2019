1a23fcb2359f8601ecffb0ab390638a8c15867a0
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-06-28 10:08:25 +0200

    implemented the component home and its css

--- front/src/components/home/Home.jsx
+++ front/src/components/home/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Container } from 'reactstrap';
-
+import './Home.css';
 
 function Home() {
-  return (
-    <Container>
-      <h1>Welcome to the inter-campus hackathon competition</h1>
-      <h3>Present by the Hacka5 team!</h3>
-    </Container>
-  );
+	return (
+		<body>
+			<Container>
+				<header>
+					<h1 className='titre'>Welcome to the inter-campus hackathon competition</h1>
+					<h3 className='sousTitre'>Present by the Hacka5 team!</h3>
+				</header>
+			</Container>
+		</body>
+	);
 }
 
 export default Home;
